21dd36d2fc40df7a43f1e3542ee1604e021726b6
bajelidze <irakli.bajelidze@exasol.com>
2022-04-06 10:05:12 +0200

    Fix tmux

--- bootstrap.sh
+++ bootstrap.sh
@@ -57,8 +57,11 @@ install_nvim_plugins() {
 }
 
 install_dwm_tmux() {
-  mv dwm.tmux/bin/dwm.tmux "$HOME/.local/bin"
-  mv dwm.tmux/lib/dwm.tmux "$HOME/.local/lib"
+  wget 'https://github.com/nelsonenzo/tmux-appimage/releases/download/3.2a/tmux.appimage' -O "$HOME/bin/tmux" \
+    && chmod +x "$HOME/bin/tmux"
+
+  mv dwm.tmux/bin/dwm.tmux "$HOME/bin"
+  mv dwm.tmux/lib/dwm.tmux "$HOME/lib"
 }
 
 create_local() {
